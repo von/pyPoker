03cb1eb8ef7818eabff06810a4748200d7a35cef
vwelch <vwelch@abe2e4a2-2663-0410-a2dd-d8fae269ebf0>
2006-07-12 01:11:21 +0000

    Added board-count app.
    git-svn-id: svn+ssh://cvs.vwelch.com/home/vwelch/SVN/pyPoker/trunk@20 abe2e4a2-2663-0410-a2dd-d8fae269ebf0

---
+++ apps/board-count.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+######################################################################
+#
+# Simulate chances five card board making flushes, pairs, etc.
+#
+# $Id$
+#
+######################################################################
+
+from optparse import OptionParser
+from pyPoker.Hand import Board, Hand
+from pyPoker.Cards import Suit, Rank
+from pyPoker.Deck import Deck
+import sys
+
+######################################################################
+
+usage = "usage: %prog [<options>]"
+parser = OptionParser(usage)
+parser.add_option("-n", "--numDeals", type="int", dest="numDeals",
+		  default=1000, help="number of deals to simulate (Default is 100)")
+parser.add_option("-p", "--showProgress", action="store_true",
+		  dest="showProgress", default=False, help="show progress")
+(options, args) = parser.parse_args()
+
+######################################################################
+
+print "Testing for %d deals" % options.numDeals
+
+possibleFlushes = 0
+
+flushCounts = [ 0, 0, 0, 0, 0, 0]
+rankCounts = [ 0, 0, 0, 0, 0]
+
+for deal in range(options.numDeals):
+    deck = Deck()
+    deck.shuffle()
+    board = Board()
+    deck.deal(board, 5)
+    # Count flushes
+    for suit in Suit.suits:
+	count = board.suitCount(suit)
+	flushCounts[count] += 1
+    # Count pairs, trips and quads
+    for rank in Rank.ranks:
+	count = board.rankCount(rank)
+	rankCounts[count] += 1
+    if options.showProgress and (deal % 20 == 0):
+	sys.stdout.write(".")
+	sys.stdout.flush()
+
+if options.showProgress:
+    print
+
+for index in range(len(flushCounts)):
+    print "%d-flush ocurrances: %d" % (index,
+				      flushCounts[index])
+
+print "Board pairs: %d" % rankCounts[2]
+print "Board trips: %d" % rankCounts[3]
+print "Board quads: %d" % rankCounts[4]
+
+sys.exit(0)
