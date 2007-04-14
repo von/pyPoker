6f6ae62fb09f2d7eaecf7422e06e7f155406efb4
vwelch <vwelch@abe2e4a2-2663-0410-a2dd-d8fae269ebf0>
2007-04-14 22:42:06 +0000

    Added test for creating mulitple HoldEmHands.
    git-svn-id: svn+ssh://cvs.vwelch.com/home/vwelch/SVN/pyPoker/trunk@21 abe2e4a2-2663-0410-a2dd-d8fae269ebf0

--- unittests/test-Deck.py
+++ unittests/test-Deck.py
@@ -6,7 +6,7 @@
 # $Id$
 ######################################################################
 
-from pyPoker.Hand import Hand
+from pyPoker.Hand import Hand, HoldEmHand
 from pyPoker.Deck import Deck
 import unittest
 
@@ -37,6 +37,15 @@ class TestSequenceFunctions(unittest.TestCase):
 	deck.deal(hands, 5)
 	for hand in hands:
 	    self.assertEquals(len(hand), 5)
+
+    def testCreateHands(self):
+	"""Test creating multiple hands."""
+	deck = Deck()
+	deck.shuffle()
+	hands = deck.createHands(8, handClass=HoldEmHand)
+	self.assertEquals(len(hands), 8)
+	for hand in hands:
+	    self.assertEquals(len(hand), 2)
 
 if __name__ == "__main__":
     unittest.main()
