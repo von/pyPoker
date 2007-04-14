99b496b9509eb46aba16315442557ffda2e6b811
vwelch <vwelch@abe2e4a2-2663-0410-a2dd-d8fae269ebf0>
2007-04-14 22:43:36 +0000

    Added pointValue() and openingHand() for OmahaHand
    git-svn-id: svn+ssh://cvs.vwelch.com/home/vwelch/SVN/pyPoker/trunk@22 abe2e4a2-2663-0410-a2dd-d8fae269ebf0

--- pyPoker/Hand.py
+++ pyPoker/Hand.py
@@ -205,6 +205,14 @@ class Board(Hand):
 	"""Return true if this board can produce a eight or better low."""
 	return (self.countEightOrLower() >= 3)
 
+    def paired(self):
+	"""Return true if board is paired."""
+	for index in range(len(self) - 1):
+	    for subindex in range(index + 1, len(self)):
+		if self[index].rank == self[subindex].rank:
+		    return True
+	return False
+
 class FiveCardBoard(Board):
     """A hand representing a five-card board."""
     pass
@@ -297,4 +305,93 @@ class OmahaHand(CommunityCardHand):
     def eightLowPossible(self):
 	"""Return true if this hand can have a eight or better low."""
 	return (self.countEightOrLower() >= 2)
+
+    def pointValue(self):
+	"""Return the point value of the hand as defined by:
+http://casinogambling.about.com/cs/poker/a/omahahilo_2.htm"""
+	total = 0
+	# Count pairs. A pair adds points equal to rank of card, 30 for aces.
+	# Give half-credit for trips and none for quads
+	for rank in Rank.ranks:
+	    value = rank
+	    if rank == Rank.ACE:
+		value = 30
+	    count = self.rankCount(rank)
+	    if count == 2:
+		total += value
+	    elif count == 3:
+		total += value/2
+	# Count flushes. Two-flush with Ace gives 10 points, 4 points otherwise.
+	# Half credit for three or four flush
+	for suit in Suit.suits:
+	    suitedCards = self.suitedCards(suit)
+	    if suitedCards.rankCount(Rank.ACE) > 0:
+		value = 10
+	    else:
+		value = 4
+	    if len(suitedCards) == 2:
+		total += value
+	    elif len(suitedCards) > 2:
+		total += value/2
+	# Count straights
+	# A two-straight with no or 1-card gap is two points
+	for rank in range(Rank.SIX, Rank.ACE + 1):
+	    for rank2 in range(rank - 2, rank):
+		if self.rankCount(rank) and self.rankCount(rank2):
+		    total += 2
+	# High Cards. Unpaired Ace is 4 points, unpaired King in 2 points.
+	if self.rankCount(Rank.ACE) == 1:
+	    total += 4
+	if self.rankCount(Rank.KING) == 1:
+	    total += 2
+	# Count low hands
+	# A-2 is 20 points, A-3 is 15, 2-3 and A-4 is 10.
+	# Other two babies are 5 points.
+	acesLowState = Rank.getAcesLow()
+	Rank.acesAreLow()
+	for rank in range(Rank.ACE_LOW, Rank.FIVE):
+	    for rank2 in range(rank + 1, Rank.SIX):
+		if self.rankCount(rank) and self.rankCount(rank2):
+		    if rank == Rank.ACE_LOW:
+			if rank2 == Rank.TWO:
+			    total += 20
+			elif rank2 == Rank.THREE:
+			    total += 15
+			elif rank2 == Rank.FOUR:
+			    total += 10
+		    elif rank == Rank.TWO and rank2 == Rank.THREE:
+			total += 10
+		    else:
+			total += 5
+	Rank.setAcesLow(acesLowState)
+	return total
+
+    def openingHand(self):
+	"""Is this an opening hand according to "Winning Omaha 8 Poker" by
+Tenner and Krieger?"""
+	# A2xx
+	if self.rankCount(Rank.ACE):
+	    if self.rankCount(Rank.TWO):
+		# A3xx
+		return True
+	    if self.rankCount(Rank.THREE):
+		if self.haveSuitedAce():
+		    # As3xx	
+		    return True
+		if self.bigCardCount() == 3:
+		    # A3XX
+		    return True
+	    if self.babyCount() > 2:
+		# Abbx
+		return True
+	    if self.haveSuitedAce() and self.bigCardCount() > 2:
+		# AsXXx
+		return True
+	if self.babyCount() == 4:
+	    # bbbb
+	    return True
+	if self.bigCardCount() == 4:
+	    # XXXX
+	    return True
+	return False
 
