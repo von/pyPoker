0116d41f481be726bfb4f34fd87f2f16ab17bc47
Von Welch <von@vwelch.com>
2010-08-16 13:22:01 -0500

    Utils.combinations() now uses itertools.combinations() instead of internal logic.

--- pyPoker/Utils.py
+++ pyPoker/Utils.py
@@ -1,5 +1,7 @@
 """Internal utility functions"""
 
+import itertools
+
 def assertInstance(obj, cls):
     if obj is None:
 	raise TypeError("Object is None")
@@ -34,7 +36,9 @@ def rindex(array, value):
 def combinations(set, n):
     """Arguments: set, n
 
-    Return an iterator returning all the subsets of length n from set."""
+    Return an iterator returning all the subsets of length n from set.
+    Note that unlike itertools.combinations() the returned subsets will be
+    the same class as set."""
     if len(set) < n:
 	raise IndexError("Not enough elements (asked for %d, only have %d)" %
 			 (n, len(set)))
@@ -43,40 +47,6 @@ def combinations(set, n):
 	# passed in set.
 	yield set.__class__(set)
     else:
-	setLen = len(set)
-	# iters will hold an array of n iterators that will progress through
-	# the index values of the set
-	iters = [rangeIter(setLen - n + 1)]
-	# values holds the index values of the current subset
-	values = [0] * n
-	values[0] = iters[0].next()
-	# What iterator is currently progressing
-	level = 1
-	while True:
-	    # Fill array of iterators and corresponding values up
-	    while level < n:
-		iters.append(rangeIter(values[level - 1] + 1,
-				   setLen - n + level + 1))
-		values[level]=iters[level].next()
-		level += 1
-	    yield set.__class__([set[i] for i in values])
-	    while True:
-		# Get next value at lowest level, if they iterator is done
-		# remove it from the iters array and go up a level until
-		# we find one that isn't done. while loop above will then
-		# fill array back in.
-		# If we reach level 0, then topmost iterator has completed
-		# and we're done.
-		try:
-		    values[level - 1] = iters[level - 1].next()
-		    break
-		except StopIteration:
-		    # Last iterator in array is done, remove it and go to
-		    # previous iterator.
-		    iters.pop()
-		    level -= 1
-		    if level == 0:
-			# Top-level iterator is done, so we are too
-			raise StopIteration
-		    # Loop back and try again
+        for comb in itertools.combinations(set, n):
+            yield set.__class__(comb)
 	
