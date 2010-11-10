f7dcd27c4e888d099910e914dcb13939f89a7e5f
Von Welch <von@vwelch.com>
2010-11-10 07:12:41 -0600

    Added stdout_to_pipe() and pipe_to_stdin() methods.

--- unittests/testing.py
+++ unittests/testing.py
@@ -1,5 +1,8 @@
 """Wrapper around unittest.TestCase adding functionality"""
 
+import contextlib
+import os
+import sys
 import unittest
 
 class TestCase(unittest.TestCase):
@@ -21,6 +24,52 @@ class TestCase(unittest.TestCase):
                 self.assertTrue(assert_item_function(item))
         if count is not None:
             self.assertEqual(iterator_index, count)
+
+    @contextlib.contextmanager
+    def stdout_to_pipe(self):
+        """Context manager for creating a pipe that receives anything
+        sent to stdout.
+
+        >>> with self.stdout_to_pipe() as output:
+        ...     print "Hello world!"
+        ...     self.assertEqual(output.readline(), "Hello world!\n")
+
+        output is a file object and is not closed so that calling context
+        can read any data out of it after with block closes.
+        """
+        saved_stdout = sys.stdout
+        read_fd, write_fd = os.pipe()
+        reader = os.fdopen(read_fd)
+        writer = os.fdopen(write_fd, "w", 0)  # 0 == unbuffered
+        sys.stdout = writer
+        try:
+            yield reader
+        finally:
+            sys.stdout = saved_stdout
+            writer.close()
+            # Leave reader for calling context to close so they can read
+            # any data off of it first.
+
+    @contextlib.contextmanager
+    def pipe_to_stdin(self):
+        """Context manager for creating a pipe that sends anything it
+        receives to stdin.
+
+        >>> with self.pipe_to_stdin() as input:
+        ...     input.write("Hello world!\n")
+        ...     self.assertEqual(sys.stdin.readline(), "Hello world!\n")
+        """
+        saved_stdin = sys.stdin
+        read_fd, write_fd = os.pipe()
+        reader = os.fdopen(read_fd)
+        writer = os.fdopen(write_fd, "w", 0)  # 0 == unbuffered
+        sys.stdin = reader
+        try:
+            yield writer
+        finally:
+            sys.stdin = saved_stdin
+            reader.close()
+            writer.close()
 
 def main(**kwargs):
     """Wrapper around unittest.main()"""
